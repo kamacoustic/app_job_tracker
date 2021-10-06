77010d3b825654a389900f052eba3eb54aa9fcd3
kamacoustic <75150502+kamacoustic@users.noreply.github.com>
2021-10-06 11:31:03 -0400

    Initial commit, just situated example position component. Barely styled.

---
+++ src/components/Position.js
@@ -0,0 +1,20 @@
+import react from "react"; 
+import "./Position.css"
+
+const Position = () => {
+
+
+
+    return (
+        <div className="pos_container">
+            <div className="pos_content">
+                <h1 id="title">Trainer</h1>
+                <h2 id="company">SIC INC.</h2>
+                <p id="summary">Seeking technical trainer</p>
+                <p id="date">10-6-2021</p>
+            </div>
+        </div>
+    )
+}
+
+export default Position
